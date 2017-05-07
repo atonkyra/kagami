67e610ef377452cf82f6162fa8bfd7b8d2ef986c
Antti Tönkyrä <daedalus@pingtimeout.net>
2017-05-08 02:18:29 +0300

    more work and error checking

--- src/main.rs
+++ src/main.rs
@@ -17,46 +17,70 @@ fn main() {
         let msg = socket.recv_msg(0).unwrap();
         let result = msg.as_str().unwrap();
         let cmd: serde_json::Value = serde_json::from_str(result).unwrap();
+        let resp;
         if cmd["action"] == "walk" {
             if let Some(ref oid) = cmd["oid"].as_str() {
                 let mut oidvec: Vec<u32> = Vec::new();
+                let mut oid_good = true;
                 for snum in oid.split(".") {
                     let res: Result<u32, std::num::ParseIntError> = snum.parse();
                     if res.is_err() {
-                        let resp = json!({
-                            "status": "failure"
-                        });
-                        while socket.send_str(&resp.to_string(), 0).is_err() {}
-                        continue;
+                        oid_good = false;
+                        break;
                     }
                     let num = res.unwrap();
                     oidvec.push(num);
                 }
-                let oid_slice = oidvec.as_slice();
-                let agent_addr = "172.24.2.1:161";
-                let community = b"m4kai";
-
-                let resp_values = snmp_querier::walk_oid(agent_addr, community, oid_slice);
-
-                //let resp_values: Vec<serde_json::Value> = Vec::new();
-                let resp = json!({
-                    "status": "ok",
-                    "data": resp_values
-                });
-                while socket.send_str(&resp.to_string(), 0).is_err() {}
+                if !oid_good {
+                    resp = json!({
+                        "status": "error",
+                        "error": "oid parse error"
+                    });
+                } else {
+                    let oid_slice = oidvec.as_slice();
+                    if let Some(ref agent_addr) = cmd["address"].as_str() {
+                        if let Some(ref community) = cmd["community"].as_str() {
+                            let community_bytes = community.as_bytes();
+                            match snmp_querier::walk_oid(agent_addr, community_bytes, oid_slice) {
+                                Ok(v) => {
+                                    resp = json!({
+                                        "status": "ok",
+                                        "data": v
+                                    });
+                                },
+                                Err(v) => {
+                                    resp = json!({
+                                        "status": "error",
+                                        "error": v
+                                    });
+                                }
+                            };
+                        } else {
+                            resp = json!({
+                                "status": "error",
+                                "error": "missing or invalid address"
+                            });
+                        }
+                    } else {
+                        resp = json!({
+                            "status": "error",
+                            "error": "missing or invalid address"
+                        });
+                    }
+                }
             } else {
-                let resp = json!({
-                    "status": "failure"
+                resp = json!({
+                    "status": "error",
+                    "error": "missing or invalid oid field"
                 });
-                while socket.send_str(&resp.to_string(), 0).is_err() {}
             }
         } else {
-            let resp = json!({
-                "status": "failure"
+            resp = json!({
+                "status": "error",
+                "error": "missing or invalid action"
             });
-            while socket.send_str(&resp.to_string(), 0).is_err() {}
         }
-        //println!("jee {}", cmd);
+        while socket.send_str(&resp.to_string(), 0).is_err() {}
     }
 
 }
--- src/snmp_querier.rs
+++ src/snmp_querier.rs
@@ -6,14 +6,17 @@ use self::snmp::{SyncSession, ObjectIdentifier};
 use std::vec::Vec;
 use std::str;
 
-pub fn walk_oid(addr: &str, community: &[u8], start_oid: &[u32]) -> Vec<serde_json::Value> {
+pub fn walk_oid(addr: &str, community: &[u8], start_oid: &[u32]) -> Result<Vec<serde_json::Value>,String> {
     let mut ret: Vec<serde_json::Value> = Vec::new();
 
     let snmp_timeout = Duration::from_secs(1);
     let snmp_bulksize = 20;
     let start_oid_slice = start_oid;
     let start_oid_slice_len = start_oid_slice.len();
-    let mut sess = SyncSession::new(addr, community, Some(snmp_timeout), 0).unwrap();
+    let mut sess = match SyncSession::new(addr, community, Some(snmp_timeout), 0) {
+        Ok(sess) => sess,
+        Err(err) => return Err(err.to_string()),
+    };
     let mut current_oid: Vec<u32> = start_oid.iter().map(|v| *v as u32).collect();
     let mut in_bounds = true;
     while in_bounds {
@@ -21,7 +24,13 @@ pub fn walk_oid(addr: &str, community: &[u8], start_oid: &[u32]) -> Vec<serde_js
 
         {
             let current_oid_slice = current_oid.as_slice();
-            let response = sess.getbulk(&[&current_oid_slice], 0, snmp_bulksize).unwrap();
+            let response = match sess.getbulk(&[&current_oid_slice], 0, snmp_bulksize) {
+                Ok(response) => response,
+                Err(err) => {
+                    let error = format!("{:?}", err);
+                    return Err(error);
+                }
+            };
             for (name, val) in response.varbinds {
                 let tmp_oid_slice: &mut [u32; 128] = &mut [0; 128];
 
@@ -42,7 +51,7 @@ pub fn walk_oid(addr: &str, community: &[u8], start_oid: &[u32]) -> Vec<serde_js
                 }
 
                 if !in_bounds {
-                    println!("OOB {} => {:?}", name, val);
+                    //println!("OOB {} => {:?}", name, val);
                     break;
                 } else {
                     let oid_string = format!("{}", name);
@@ -118,5 +127,5 @@ pub fn walk_oid(addr: &str, community: &[u8], start_oid: &[u32]) -> Vec<serde_js
         }
     }
 
-    return ret;
+    return Ok(ret);
 }
